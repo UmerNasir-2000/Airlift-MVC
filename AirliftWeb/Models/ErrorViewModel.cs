30f07ce219ffcf31949c93ef3578af37da368e9a
Umer Nasir <umer.nas@outlook.com>
2022-04-21 10:37:43 +0500

    Initial Setup

---
+++ AirliftWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirliftWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
